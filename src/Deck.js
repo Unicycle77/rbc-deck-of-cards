28f2d941ce311c087848166bb5daa642bbe647e0
Jeffrey Martin <unicycle77@gmail.com>
2021-07-27 16:13:56 -0700

    Style app

--- src/Deck.js
+++ src/Deck.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Card from "./Card";
 
+import "./Deck.css";
+
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
@@ -53,10 +55,11 @@ class Deck extends Component {
       )
     })
     return (
-      <div>
-        <h1>Card Dealer</h1>
-        <button onClick={this.getCard}>Get Card!</button>
-        <div className="deck">
+      <div className="Deck">
+        <h1 className="Deck-title">♦ Card Dealer ♦</h1>
+        <h2 className="Deck-title subtitle">♦ A little demo made with React ♦</h2>
+        <button className="Deck-btn" onClick={this.getCard}>Get Card!</button>
+        <div className="Deck-cardarea">
           {cards}
         </div>
       </div>
